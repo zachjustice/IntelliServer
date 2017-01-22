dc30102ba90296eb9182c56aaf072275300b7ffb
zjustice3 <zjustice3@gatech.edu>
2017-01-22 17:50:17 -0500

    Google App Configuration file.

--- api/hello.py
+++ api/hello.py
@@ -4,7 +4,7 @@ from bottle import route, run
 
 @route('/hello')
 def hello():
-        return "<h1>Hello World!</h1>"
+        return "<h1>What up!</h1>"
 
-run(host='0.0.0.0', port=8080)
+run(host='gae')
 
