cd59b7c1d219d5a42da68bd4c2b2f541c2cd5e52
Jatin Nanda <jatin.nanda1337@gmail.com>
2017-02-06 02:20:47 -0500

    Fixed dependencies for web scraper

--- IntelliServer/webscraper/populateDB.py
+++
@@ -1,8 +0,0 @@
-from recipeFinder import getUrls
-from recipeParser import parseRecipes
-from db import insertIngredientsAndRecipes
-
-urls = getUrls()
-ingredients, recipes = parseRecipes(urls)
-ing = [x.name for x in ingredients]
-insertIngredientsAndRecipes(ingredients, recipes)
