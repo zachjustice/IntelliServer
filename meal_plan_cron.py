9da1554e70b64ed5fbf2f87c55299d2271ddde4d
Jatin Nanda <jatin.nanda1337@gmail.com>
2017-03-12 12:46:55 -0400

    testing git settings

--- meal_plan_cron.py
+++ meal_plan_cron.py
@@ -4,7 +4,7 @@ from webscraper.classifier import generate_meal_plan
 from api.models import my_map
 
 #entities = my_map(lambda r: r['entity'], get_entity_pks())
-entities = [18]
+#entities = [18]
 
 for entity in entities:
     generate_meal_plan(entity, 7)
