3ee82572d98f307e72939643b9a217ada7f443f6
Jatin Nanda <jatin.nanda1337@gmail.com>
2017-04-13 15:12:37 -0400

    crontab fixes

--- meal_plan_cron.py
+++ meal_plan_cron.py
@@ -4,10 +4,9 @@ from webscraper.classifier import generate_meal_plan
 from api.models import my_map
 
 entities = my_map(lambda r: r['entity'], get_entity_pks())
-entities = [7]
 
 for entity in entities:
-    generate_meal_plan(entity, 7, timeDelta = 3)
+    generate_meal_plan(entity, 7, timeDelta = 0)
     print("Meal Plan successfully generated for entity" + str(entity))
 
 
